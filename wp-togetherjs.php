e11ce2588b820e354f6ce235a0d6a3f7af3b754e
Ryan Nielson <ryan.nielson@gmail.com>
2013-10-04 15:04:49 -0300

    Basic framework

--- wp-togetherjs.php
+++ wp-togetherjs.php
@@ -15,7 +15,7 @@
  * @since 1.0.0
  */
 
-function add_togetherjs_start_button($wp_admin_bar) {
+function wp_togetherjs_add_start_button($wp_admin_bar) {
 	$args = array(
 		'id'    => 'start_together_js_button',
 		'title' => '<a href="#" id="start-togetherjs" class="togetherjs-button" onclick="TogetherJS(this); return false;">Start TogetherJS</a>',
@@ -27,13 +27,45 @@ function add_togetherjs_start_button($wp_admin_bar) {
 	$wp_admin_bar->add_node( $args );
 }
 
-function enqueue_wp_togetherjs_styles_and_scripts() {
+function wp_togetherjs_enqueue_styles_and_scripts() {
 	wp_enqueue_style("wp_togetherjs_style", plugins_url("assets/wp-togetherjs.css", __FILE__ ));
 	wp_enqueue_script("wp_togetherjs_script", plugins_url("assets/wp-togetherjs.js", __FILE__ ), array("jquery"));
 	wp_enqueue_script("togetherjs_script", "https://togetherjs.com/togetherjs-min.js");
 }
 
-add_action("wp_enqueue_scripts", "enqueue_wp_togetherjs_styles_and_scripts");
-add_action("admin_enqueue_scripts", "enqueue_wp_togetherjs_styles_and_scripts");
-add_action("login_enqueue_scripts", "enqueue_wp_togetherjs_styles_and_scripts");
-add_action("admin_bar_menu", "add_togetherjs_start_button", 999);
+function wp_togetherjs_add_menu_item() {
+
+}
+
+function wp_togetherjs_add_settings_link($links) {
+
+}
+
+function wp_togetherjs_register_settings() {
+
+}
+
+function wp_togetherjs_main_settings() {
+
+}
+
+function wp_togetherjs_settings_field() {
+
+}
+
+function wp_togetherjs_validate_field() {
+
+}
+
+function wp_togetherjs_settings_page() {
+
+}
+
+add_action("wp_enqueue_scripts", "wp_togetherjs_enqueue_styles_and_scripts");
+add_action("admin_enqueue_scripts", "wp_togetherjs_enqueue_styles_and_scripts");
+add_action("login_enqueue_scripts", "wp_togetherjs_enqueue_styles_and_scripts");
+add_action("admin_bar_menu", "wp_togetherjs_add_start_button", 999);
+
+add_action("admin_init", "wp_togetherjs_register_settings");
+add_action("admin_menu", "wp_togetherjs_add_menu_item");
+add_filter("plugin_action_links_" . plugin_basename(__FILE__), "wp_togetherjs_add_settings_link");
