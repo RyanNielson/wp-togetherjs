380d96f58abc5bfaf46a5e432546b333993f6f1f
Ryan Nielson <ryan.nielson@gmail.com>
2013-10-04 15:51:46 -0300

    git branch

--- wp-togetherjs.php
+++ wp-togetherjs.php
@@ -34,31 +34,63 @@ function wp_togetherjs_enqueue_styles_and_scripts() {
 }
 
 function wp_togetherjs_add_menu_item() {
-
+	add_options_page("WP TogetherJS", "WP TogetherJS", "manage_options", "wp_togetherjs", "wp_togetherjs_settings_page");
 }
 
 function wp_togetherjs_add_settings_link($links) {
-
+	$settings_link = "<a href='options-general.php?page=wp_togetherjs_settings'>Settings</a>";
+	array_push($links, $settings_link);
+	return $links;
 }
 
 function wp_togetherjs_register_settings() {
-
+		add_settings_section("wp_togetherjs_main_settings", "Modify plugin settings", "wp_togetherjs_main_settings", "wp_togetherjs_settings");
+		add_settings_field("wp_togetherjs_settings_field" , "Settings field:", "wp_togetherjs_settings_field", "wp_togetherjs_settings" , "wp_togetherjs_main_settings");
+		register_setting("wp_togetherjs_settings", "wp_togetherjs_settings_field" , "wp_togetherjs_validate_field");
 }
 
 function wp_togetherjs_main_settings() {
-
+	echo '<p>Change these settings ot customise your plugin.</p>';
 }
 
 function wp_togetherjs_settings_field() {
 
+	$option = get_option("wp_togetherjs_settings_field");
+
+	$data = '';
+	if( $option && strlen( $option ) > 0 && $option != '' ) {
+		$data = $option;
+	}
+
+	echo '<input id="slug" type="text" name="wp_togetherjs_settings_field" value="' . $data . '"/>
+			<label for="slug"><span class="description">Description of settings field</span></label>';
 }
 
-function wp_togetherjs_validate_field() {
-
+function wp_togetherjs_validate_field($slug) {
+	if($slug && strlen($slug) > 0 && $slug != '') {
+		$slug = urlencode( strtolower(str_replace(' ' , '-' , $slug )));
+	}
+	return $slug;
 }
 
 function wp_togetherjs_settings_page() {
+?>
 
+	<div class="wrap">
+		<div class="icon32" id="icon-options-general"><br/></div>
+		<h2>WP TogetherJS Settings</h2>
+		<form method="post" action="options.php" enctype="multipart/form-data">
+
+			<?php settings_fields("wp_togetherjs_settings"); ?>
+			<?php do_settings_sections("wp_togetherjs_settings"); ?>
+
+			<p class="submit">
+				<input name="Submit" type="submit" class="button-primary" value="Save Settings" />
+			</p>
+		</form>
+	</div>
+
+<?php
 }
 
 add_action("wp_enqueue_scripts", "wp_togetherjs_enqueue_styles_and_scripts");
